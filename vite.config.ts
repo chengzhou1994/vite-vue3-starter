d97841f40f6a6577b593fb32418ca7c5e43b248c
成舟 <1738248438@qq.com>
2021-09-02 17:12:50 +0800

    🎉 init commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './', // 设置打包路径
+  // 打包配置
+  build: {
+    target: 'modules',
+    outDir: 'dist', //指定输出路径
+    assetsDir: 'assets', // 指定生成静态资源的存放路径
+    minify: 'terser' // 混淆器，terser构建后文件体积更小
+  },
+  server: {
+    port: 4000, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
+  }
+})
